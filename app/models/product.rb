d32243af4081ec0baee70087cd6c8ed0ecbd8823
Manish Singh <singhmanishr720@gmail.com>
2024-04-03 11:57:11 +0530

    gstorageadded

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
     has_one_attached :image
-
+    after_create :upload_image_to_gcs
+   
+    
     validates :title_name, presence: true, length: { maximum: 255 }
     validates :title_description, presence: true
     validates :title_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :image, presence: true
+
+    private
+    def upload_image_to_gcs
+        if image.attached?
+          self.image = image.service_url
+          save
+        end
+    end
 end
